75213ac7b49f98bc53b8877b943745cfc2f0387d
TheoLomeuBraga <theolomeu@gmail.com>
2023-07-10 11:11:04 -0300

    a

--- simple_launcher_bar.py
+++ simple_launcher_bar.py
@@ -9,19 +9,12 @@ import os
 
 
 def resize_icon(icon_name, width, height):
-    # Create a themed icon
     themed_icon = Gio.ThemedIcon.new(icon_name)
-
-    # Load the icon
     icon_info = Gtk.IconTheme.get_default().lookup_by_gicon(themed_icon, 48, Gtk.IconLookupFlags.GENERIC_FALLBACK)
 
     if icon_info:
-        # Get the pixbuf
         pixbuf = icon_info.load_icon()
-
-        # Scale the pixbuf
         scaled_pixbuf = pixbuf.scale_simple(width, height, GdkPixbuf.InterpType.BILINEAR)
-
         return scaled_pixbuf
 
     return None
@@ -37,14 +30,16 @@ def getResolution():
     ret[0] = geometry.width
     ret[1] = geometry.height
     return ret
+    
 
 class MyWindow(Gtk.Window):
+
+    
 
     def __init__(self):
         Gtk.Window.__init__(self, title="launchers")
 
         resolution = getResolution()
-        
 
         if side == Sides.LEFT:
             self.box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
@@ -69,9 +64,9 @@ class MyWindow(Gtk.Window):
             button.set_size_request(size, size)
             button.set_tooltip_text(l.name)
             button.connect("clicked", launcher_button, l.command)
-
-
             self.box.pack_start(button, True, True, 0)
+
+        
 
 win = MyWindow()
 win.connect("destroy", Gtk.main_quit)
